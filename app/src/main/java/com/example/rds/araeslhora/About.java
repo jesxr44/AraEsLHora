55238ad98cf5f1ea2f9a4c02f1ed3ae7ed9c760e
Albert <Albertpa@gmail.com>
2014-10-04 11:20:35 +0200

    - iconos agregados - añadidas algunas frases - cambio de posición de No+frase en el layout

--- app/src/main/java/com/example/rds/araeslhora/About.java
+++ app/src/main/java/com/example/rds/araeslhora/About.java
@@ -9,6 +9,5 @@ public class About extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
-
     }
 }

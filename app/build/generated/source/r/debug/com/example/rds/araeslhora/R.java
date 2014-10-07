9e69ad2317b89d05b289da535d282307cefa8f35
jesxr44 <jesxr44@gmail.com>
2014-10-07 20:13:34 +0200

    añadido style again

--- app/build/generated/source/r/debug/com/example/rds/araeslhora/R.java
+++ app/build/generated/source/r/debug/com/example/rds/araeslhora/R.java
@@ -16,7 +16,7 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int mainbutton=0x7f020001;
+        public static final int styleagain=0x7f020001;
         public static final int stylebuttons=0x7f020002;
     }
     public static final class id {
